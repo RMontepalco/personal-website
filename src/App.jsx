fde780b2c2be5cf6f97c1f517db462f2491c42ef
RMontepalco <montepalco.ross@gmail.com>
2024-07-01 17:36:34 -0700

    Created Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import Navbar from './components/Navbar'
+
 export default function App() {
   return (
     <div>
+    <Navbar />
       <p>The beginning of my personal website using ReactJS and Vite.</p>
     </div>
   )
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+	return (
+		<nav className="navbar">
+			<p>RMontepalco</p>
+			<a href="">Home</a>
+			<a href="">About</a>
+			<a href="">Portfolio</a>
+			<a href="">Contact</a>
+		</nav>
+	)
+}
